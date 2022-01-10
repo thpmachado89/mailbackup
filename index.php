4a244c4752c845bf345e73dacf1129adbcfbdd5b
root <root@thiagopro11.localdomain>
2022-01-10 01:00:09 -0300

    Pastas Separando por pastas

--- index.php
+++ index.php
@@ -6,27 +6,57 @@ ini_set('max_execution_time', '-1');
 
 $mbox = imap_open( "{".$host.":".$port."/novalidate-cert}INBOX" , $username, $password );
 
-if( $mbox ) {
-   
-    $num = imap_num_msg($mbox);
+$list = imap_list($mbox, "{".$host."}", "*");
 
-    echo "Total de mensagens: " . $num . "<br>";
-         
-    $email_number = 2;
+foreach($list as $folder){
 
+    $folder = str_replace("{".$host."}", "", $folder);
 
-    for($i=140;$i<=$num;$i++){
-
-        $message_count = imap_num_msg($mbox);
-
-        $headers = imap_fetchheader($mbox, $i);
-       
-        $body = imap_body($mbox, $i, 1.2);
-    
-        file_put_contents("C:/projects/mailbackup/eml/".$i.".eml", $headers . "\n" . $body);
-
-    }
-    
-    imap_close($mbox);
+    getMail($folder);
 
 }
+
+function getMail($folder){
+
+    include("credentials.php");
+    
+    $mbox = imap_open( "{".$host.":".$port."/novalidate-cert}".$folder , $username, $password );
+
+    $folder = $folder . "/"; 
+
+    if( $mbox ) {
+   
+        $num = imap_num_msg($mbox);
+    
+        echo "Total de mensagens: " . $num . "<br>";
+             
+        $email_number = 2;
+    
+    
+        for($i=1;$i<=$num;$i++){
+    
+            $message_count = imap_num_msg($mbox);
+    
+            $headers = imap_fetchheader($mbox, $i);
+           
+            $body = imap_body($mbox, $i, 1.2);
+    
+            $directory = "C:/projects/mailbackup/eml/";
+    
+            if (!file_exists($directory.$folder)) {
+    
+                mkdir($directory.$folder, 0777, true);
+            
+            }
+        
+            file_put_contents($directory.$folder.$i.".eml", $headers . "\n" . $body);
+    
+        }
+        
+        imap_close($mbox);
+    
+    }
+
+}
+
+
